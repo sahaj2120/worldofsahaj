656706e1aaa28ced64fad91f9ecf197cbf4c93b2
sahaj2120 <70460669+sahaj2120@users.noreply.github.com>
2021-03-22 11:51:09 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,438 @@
+<!doctype html>
+<html lang="en">
+  
+  
+  
+  <head>
+  
+  
+		<link rel="icon" href="Icons/favicon.png" sizes="96x96" type="image/png">
+			
+			<link rel="icon" href="Icons/favicon-coast.png" sizes="228x228" type="image/png">
+			
+			<link rel="apple-touch-icon-precomposed" href="Icons/apple-touch-icon-152×152-precomposed.png" sizes="152x152" type="image/png">
+			
+			<link rel="apple-touch-icon-precomposed" href="Icons/apple-touch-icon-144×144-precomposed.png" sizes="144x144" type="image/png">
+			
+			<link rel="apple-touch-icon-precompose" href="Icons/apple-touch-icon-76×76-precomposed.png" sizes="76x76" type="image/png">
+			
+			<link rel="apple-touch-icon-precompose" href="Icons/apple-touch-icon-72×72-precomposed.png" sizes="72x72" type="image/png">
+			
+			<link rel="apple-touch-icon-precompose" href="Icons/apple-touch-icon-120x120-precomposed.png" sizes="120x120" type="image/png">
+			
+			<link rel="apple-touch-icon-precompose" href="Icons/apple-touch-icon-114x114-precomposed.png" sizes="114x114" type="image/png">
+  
+  
+  
+  
+  
+  
+  
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+   
+
+
+		<title>Submit Your Photos</title>
+		
+		<style type="text/css">
+		
+		
+	
+			.navbar-blue .navbar-toggler-icon {
+					background-image: url("images/menu.png");
+				}
+		
+			
+			.navbar-brand {
+			
+				padding-left:0px;
+			
+			}
+		
+			.nav-item {
+			
+				padding-left:20px;
+			
+			}
+		
+			.dropdown-menu {
+			
+				background-color:#24292E;
+			
+			}
+			
+			.dropdown-item {
+			
+				color:white;
+			
+			}
+		
+			.navbar-branddd {
+				
+				padding-left:20px;
+			
+			}
+		
+		
+		
+			.jumbotron-fluid {
+			
+			
+				
+				text-align:center;
+				padding-top:10px;
+				padding-bottom:10px;
+			
+			}
+			
+			.display-4 {
+			
+				padding-top:10px;
+				font-family: anton,sans-serif;
+				font-size: 2.5rem;
+				font-weight:bold;
+				color:#24292E;
+				
+			
+			}
+			
+			.lead {
+			
+			
+				font-family: anton,sans-serif;
+				font-size: 18px;
+				color:#24292E;
+			
+			}
+			
+			
+			.container {
+			
+				text-align:center;
+				background-color:
+				
+				
+				
+			
+			
+			
+			}
+			
+			
+			
+			
+			
+			.card-title {
+			
+				text-align:center;
+				font-weight:bold;
+				font-family: anton,sans-serif;
+				font-size: 2rem;
+				color:white;
+				
+		
+			}
+			
+			.card {
+			
+				background-color:#E9ECEF;
+				border:2px solid #E9ECEF;
+				margin-left:20px;
+				transition: transform .2s;
+			
+			
+				
+			
+			}
+			
+			.card:hover {
+			
+					-ms-transform: scale(1.05); /* IE 9 */
+					-webkit-transform: scale(1.05); /* Safari 3-8 */
+						transform: scale(1.05); 
+			
+			}
+		
+			h5 a {
+			
+				color:white;
+			
+			
+			}
+			
+			h5 a:hover {
+			
+				color:#00b4ff;
+				text-decoration:none;
+			
+			}
+			
+			
+			.breadcrumb {
+			
+				text-align:center;
+				font-weight:bold;
+				font-family: anton,sans-serif;
+				padding-left:20px;
+				background-color:none;
+				font-size:20px;
+				
+				
+			
+			}
+			
+			
+			form{
+			
+				padding-left:100px;
+			
+			
+			}
+			
+			.footer-copyright {
+			
+				text-align:center;
+				font-family:anton,sans-serif;
+				font-size: 1.2rem;
+			
+			
+			}
+			
+		
+		
+		
+		</style>
+  
+  
+  
+  
+  
+  
+  
+  </head>
+ 
+
+
+
+
+	<body style="background-color:#E9ECEF">
+	
+	
+	
+	
+	
+	<nav class="navbar navbar-expand-lg navbar-blue" style="background-color:#24292E;">
+			<a class="navbar-brand" href="index.html"><img src="images/logo.jpg"></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+       <li class="nav-item">
+        <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+      </li>
+	  
+	  
+	  
+	  <li class="nav-item">
+        <a class="nav-link" href="AboutMe.html">About Me<span class="sr-only">(current)</span></a>
+      </li>
+     
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Photos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		<a class="dropdown-item" href="Architecture.html">Architecture</a>
+          <a class="dropdown-item" href="Nature.html">Nature</a>
+          <a class="dropdown-item" href="Sunset.html">Sunset</a>
+          <a class="dropdown-item" href="Birds.html">Birds</a>
+		  <a class="dropdown-item" href="Animals.html">Animals</a>
+          <a class="dropdown-item" href="Flowers.html">Flowers</a>
+		  <a class="dropdown-item" href="SomethingElse.html">Unspecified</a>
+        </div>
+      </li>
+	  
+	  
+	   <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Wallpapers
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Mobile.html">Mobile</a>
+          <a class="dropdown-item" href="PC.html">PC</a>
+		 
+		 
+		<li class="nav-item">
+			<a class="nav-link" href="upload.html">Submit Your Photos<span class="sr-only">(current)</span></a>
+		</li>
+         
+		 
+ </li>
+     
+    </ul>
+	
+	<hr>
+	
+	
+		<a class="navbar-branddd" href="https://www.instagram.com/worldofsahaj/"><img src="images/instagram.png"></a>
+		<a class="navbar-branddd" href="https://www.facebook.com/SahajPatell/"><img src="images/facebook.png"></a>
+	
+  
+	</div>
+	</nav>
+	
+	
+
+	
+	
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+		<h1 class="display-4">Submit Your Photos</h1>
+		
+		</div>
+	</div>
+	
+	
+		
+
+		<nav aria-label="breadcrumb">
+		  <ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Submit Your Photos</li>
+		  </ol>
+		</nav>
+
+		<div>
+		
+		
+			<?php
+			
+				$target_dir = "uploads/";
+				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+				$uploadOk = 1;
+				$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+				// Check if image file is a actual image or fake image
+				if(isset($_POST["submit"])) {
+				  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+				  if($check !== false) {
+					echo " </br> File is an image - " . $check["mime"] . ".";
+					$uploadOk = 1;
+				  } else {
+					echo " </br> File is not an image.";
+					$uploadOk = 0;
+				  }
+				}
+
+				// Check if file already exists
+				if (file_exists($target_file)) {
+				  echo " </br> Sorry, file already exists.";
+				  $uploadOk = 0;
+				}
+
+				// Check file size
+				if ($_FILES["fileToUpload"]["size"] > 1500000) {
+				  echo " </br> Sorry, your file is too large.";
+				  $uploadOk = 0;
+				}
+
+				// Allow certain file formats
+				if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+				 ) {
+				  echo " </br> Sorry, only JPG, JPEG & PNG  files are allowed.";
+				  $uploadOk = 0;
+				}
+
+				// Check if $uploadOk is set to 0 by an error
+				if ($uploadOk == 0) {
+				  echo " </br> Sorry, your file was not uploaded. </br>";
+				// if everything is ok, try to upload file
+				} else {
+				  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+					echo " </br> The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded. </br> ";
+				  } else {
+					echo "</br> Sorry, there was an error uploading your file. </br>";
+				  }
+				}
+				
+				
+				
+				$server = "localhost";
+				$username = "root";
+				$password = "";
+				
+				$con = mysqli_connect($server,$username,$password);
+				
+				
+				if(!$con){
+					
+					die(" </br> connection to this database failed due to" .mysqli_connect_error());
+					
+				}
+				//echo " </br> Successfully Connected To The Database";
+				
+				
+				
+				
+				
+				
+				
+				
+				$firstname = $_POST['firstname'];
+				$lastname = $_POST['lastname'];
+				$email = $_POST['email'];
+				
+				
+				$sql = "INSERT INTO `upload`.`submitfiles` (`firstname`, `lastname`, `email`) VALUES ('$firstname', '$lastname', '$email');";
+				//echo  $sql;
+				
+				if($con->query($sql) == true){
+					
+					//echo "</br> Successfully Inserted";
+					
+				}
+				else{
+					
+					echo "</br> ERROR: $sql </br> $con->error";
+					
+				}
+				
+				$con->close();
+				
+			
+			?>
+
+
+		</div>
+	
+	
+	
+	
+	<p class="footer-copyright">©
+							2021	<a id="copyright" href="https://worldofsahaj.netlify.app/">Worldofsahaj</a>
+	</p>
+	
+	
+
+
+   
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  
+  
+  
+  </body>
+
+
+
+
+</html>
